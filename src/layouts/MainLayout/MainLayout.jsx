2f4798bf82adc68932791380ae7fa1381f6d2dd2
Elias Jimenez Diz <eliasj.diz@gmail.com>
2023-05-31 10:18:47 -0500

    footer 100% responsive, falta ubicar el contenido, que full step sea primero

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header/Header.jsx'
 import { Outlet } from 'react-router-dom'
+import Footer from '../../components/Footer/Footer.jsx'
 
 export default function MainLayout() {
   return (
     <div class="main-container">
     <Header/>
+    <Footer />
     <Outlet/>
     </div>
   )
